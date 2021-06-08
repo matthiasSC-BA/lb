d1f614c63850140ad8172bc8200b22f7800f765b
matthiasSC-BA <55184044+matthiasSC-BA@users.noreply.github.com>
2021-06-08 19:26:41 +0200

    Update main.tf

--- vraVm/main.tf
+++ vraVm/main.tf
@@ -20,7 +20,7 @@ data "vsphere_datastore" "datastore" {
 }
 
 data "vsphere_resource_pool" "pool" {
-  name          = "WLD01"
+  name          = "testpool"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 /*
