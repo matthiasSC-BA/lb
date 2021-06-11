5865d93e145699ec02febb735b524d45966f1ae8
matthiasSC-BA <55184044+matthiasSC-BA@users.noreply.github.com>
2021-06-11 23:37:56 +0200

    Update main.tf

--- vraVm/main.tf
+++ vraVm/main.tf
@@ -25,48 +25,25 @@ data "vsphere_datacenter" "dc" {
 
 data "vsphere_compute_cluster" "cluster" {
   name          = "Workload_Cluster"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_datastore" "datastore" {
   name          = "DemoDS"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_network" "network" {
   name          = "DominikProjekt"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 # Get data about the image you're going to clone from.
 data "vsphere_virtual_machine" "image" {
     name = "ubuntuTemplate"
-    datacenter_id = "${data.vsphere_datacenter.dc.id}"
+    datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-# # Configure the cloud-init data you're going to use
-# data "template_cloudinit_config" "cloud-config" {
-#     gzip          = true
-#     base64_encode = true
- 
-#     # This is your actual cloud-config document.  You can actually have more than
-#     # one, but I haven't much bothered with it.
-#     part {
-#       content_type = "text/cloud-config"
-#       content      = <<-EOT
-#                      #cloud-config
-#                      users:
-#                        - name: "${var.username}"
-#                          plain_text_passwd: "${var.password}"
-#                          lock_passwd: false
-#                          ssh_pwauth: "yes"
-#                          sudo:
-#                            - "ALL=(ALL) NOPASSWD:ALL"
-#                          groups: sudo
-#                          shell: /bin/bash
-#                      EOT
-#     }
-# } 
 
 data "template_file" "cloud-init" {
   template = file("userdata.yaml")
@@ -85,21 +62,33 @@ data "template_cloudinit_config" "cloud-init" {
   }
 }
 
+data "template_file" "meta_init" {
+  template = <<EOF
+{
+        "local-hostname": "$${local_hostname}"
+}
+EOF
+ 
+  vars = {
+    local_hostname = var.vm_name
+  }
+}
+
 resource "vsphere_virtual_machine" "vm" {
   name             = var.vm_name
 
-  resource_pool_id = "${data.vsphere_compute_cluster.cluster.resource_pool_id}"
-  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
 
   network_interface {
-    network_id = "${data.vsphere_network.network.id}"
+    network_id = data.vsphere_network.network.id
   }
 
   num_cpus = 1
   memory   = 1024
-  guest_id = "${data.vsphere_virtual_machine.image.guest_id}"
+  guest_id = data.vsphere_virtual_machine.image.guest_id
   clone {
-    template_uuid = "${data.vsphere_virtual_machine.image.id}"
+    template_uuid = data.vsphere_virtual_machine.image.id
   }
   cdrom {
     client_device = true
@@ -111,7 +100,9 @@ resource "vsphere_virtual_machine" "vm" {
   }
   wait_for_guest_net_timeout    = 5
   
-  extra_config = {
+  extra_config = {    
+    "guestinfo.metadata" = base64gzip(data.template_file.meta_init.rendered)
+    "guestinfo.metadata.encoding" = "gzip+base64"
     "guestinfo.userdata" = base64gzip(data.template_file.cloud-init.rendered)
   }
 #   vapp {
