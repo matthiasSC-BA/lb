e5d66eed67e4347e04e99ecca62fbcad8b023ab2
matthiasSC-BA <55184044+matthiasSC-BA@users.noreply.github.com>
2021-06-11 15:42:16 +0200

    Update main.tf

--- vraVm/main.tf
+++ vraVm/main.tf
@@ -3,6 +3,16 @@ variable "vm_name" {
   description = "VM Name"
 }
 
+variable "hostname" {
+  type        = string
+  description = "hostname"
+}
+
+variable "domain" {
+  type        = string
+  description = "domain"
+}
+
 
 provider "vsphere" {
 
@@ -14,28 +24,50 @@ data "vsphere_datacenter" "dc" {
   name = "Noris"
 }
 
+data "vsphere_compute_cluster" "cluster" {
+  name          = "WLD01"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
 data "vsphere_datastore" "datastore" {
   name          = "DemoDS"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
-data "vsphere_resource_pool" "pool" {
-  name          = "testpool"
+data "vsphere_network" "network" {
+  name          = "DominikProjekt"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
-data "vsphere_network" "network" {
-  name          = "VM Network"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
-}
+# Configure the cloud-init data you're going to use
+data "template_cloudinit_config" "cloud-config" {
+    gzip          = true
+    base64_encode = true
+ 
+    # This is your actual cloud-config document.  You can actually have more than
+    # one, but I haven't much bothered with it.
+    part {
+      content_type = "text/cloud-config"
+      content      = <<-EOT
+                     #cloud-config
+                     users:
+                       - name: 'mat'
+                         plain_text_passwd: 'VMware1!'
+                         lock_passwd: false
+                         ssh_pwauth: 'yes'
+                         sudo:
+                           - 'ALL=(ALL) NOPASSWD:ALL'
+                         groups: sudo
+                         shell: /bin/bash
+                     EOT
+    }
+} 
 
 resource "vsphere_virtual_machine" "vm" {
   name             = var.vm_name
 
-  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
+  resource_pool_id = "${data.vsphere_compute_cluster.cluster.resource_pool_id}"
   datastore_id     = "${data.vsphere_datastore.datastore.id}"
-
-
 
   network_interface {
     network_id = "${data.vsphere_network.network.id}"
@@ -50,4 +82,12 @@ resource "vsphere_virtual_machine" "vm" {
     size  = 10
   }
   wait_for_guest_net_timeout    = -1
+  
+  extra_config {
+    "guestinfo.userdata"          = "${data.template_cloudinit_config.cloud-config.rendered}"
+    "guestinfo.userdata.encoding" = "gzip+base64"
+    "guestinfo.metadata"          = <<-EOT
+       { "local-hostname": "${var.hostname}.${var.domain}" }
+    EOT
+  }
 }
